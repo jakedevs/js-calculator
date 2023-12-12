7e3f7c713a332110d68f0e63e5d2857ae181896d
jakedevs <jakedevs@skiff.com>
2023-12-12 02:20:57 -0500

    Add html boilerplate, empty style.css, and initial js logic

---
+++ index.js
@@ -0,0 +1,23 @@
+let num1 = 0;
+let num2 = 0;
+let operator = 0;
+
+function add() {
+  return num1 + num2;
+}
+
+function subtract() {
+  return num1 - num2;
+}
+
+function multiply() {
+  return num1 * num2;
+}
+
+function divide() {
+  return num1 / num2;
+}
+
+function operate(num1, num2) {
+    
+}
