9c251b65a3b6235430fbf6f904ae13c05ae68697
jakedevs <jakedevs@skiff.com>
2023-12-13 16:21:20 -0500

    add result display, set all operators to ID "operate", add result value, get basic logic working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let num1 = undefined;
 let operator = undefined;
 let num2 = undefined;
+let result = undefined;
 
 let displaynum1 = document.querySelector("#displaynum1");
 displaynum1.textContent = num1;
@@ -11,35 +12,70 @@ displayoperator.textContent = operator;
 let displaynum2 = document.querySelector("#displaynum2");
 displaynum2.textContent = num2;
 
+let displayresult = document.querySelector("#displayresult");
+displayresult.textContent = result;
+
+let digit = document.querySelectorAll("#digit");
+
+let operators = document.querySelectorAll("#operator");
+let equal = document.querySelector("#equals");
+
+equal.addEventListener("click", () => {
+  displayresult.textContent = operate();
+  console.log(operate());
+  console.log(displayresult.textContent);
+});
+
+operators.forEach((currentop) => {
+  currentop.addEventListener("click", () => {
+    console.log(currentop.textContent);
+    operator = currentop.textContent;
+    displayoperator.textContent = currentop.textContent;
+  });
+});
+
+digit.forEach((selectednumber) => {
+  selectednumber.addEventListener("click", () => {
+    console.log(selectednumber.textContent);
+    if (num1 == undefined) {
+      num1 = selectednumber.textContent;
+      displaynum1.textContent = selectednumber.textContent;
+    } else {
+      num2 = selectednumber.textContent;
+      displaynum2.textContent = selectednumber.textContent;
+    }
+  });
+});
+
 function add() {
-  return num1 + num2;
+  return parseInt(num1) + parseInt(num2);
 }
 
 function subtract() {
-  return num1 - num2;
+  return parseInt(num1) - parseInt(num2);
 }
 
 function multiply() {
-  return num1 * num2;
+  return parseInt(num1) * parseInt(num2);
 }
 
 function divide() {
-  return num1 / num2;
+  return parseInt(num1) / parseInt(num2);
 }
 
 function operate() {
   switch (operator) {
     case "+":
-      add();
+      return add();
       break;
     case "-":
-      subtract();
+      return subtract();
       break;
     case "*":
-      multiply();
+      return multiply();
       break;
     case "/":
-      divide();
+      return divide();
       break;
   }
 }
